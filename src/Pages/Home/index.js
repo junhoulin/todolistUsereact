e4e358034eb21c8fcb063634f7faaf7c73653129
JUNHOULIN <yy6313531@gmail.com>
2022-08-29 14:13:00 +0800

    second commit

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,48 @@
+import { useState, useEffect, useRef } from "react";
+import { API_GET_DATA } from "../global/constant";
+import Edit from "./component/Edit";
+import List from "./component/List";
+import "./index.css";
+
+async function fetchData(setData) {
+    const res = await fetch(API_GET_DATA);
+    const { data } = await res.json();
+    setData(data);
+}
+
+async function fetchsetData(data) {
+    await fetch(API_GET_DATA, {
+        method: "PUT",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ data })
+    })
+}
+
+const Home = () => {
+
+    const [data, setData] = useState([]);
+    const submittingStatue = useRef(false);
+
+    useEffect(() => {
+        if (!submittingStatue.current) {
+            return
+        }
+        fetchsetData(data)
+            .then(data => submittingStatue.current = false)
+    }, [data]);
+
+    useEffect(() => {
+        fetchData(setData);
+    }, []);
+
+    return (
+        <div className="app">
+            <Edit add={setData} submittingStatue={submittingStatue} />
+            <List listData={data} deleteData={setData} submittingStatue={submittingStatue} />
+        </div>
+    );
+};
+
+export default Home;
